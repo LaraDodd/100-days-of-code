879d3a8182d4618ad176cbf408e9a01d4e6e8fb5
Lara Dodd <laradodd97@gmail.com>
2022-12-13 12:04:19 +0000

    using classes to define entire code

--- quiz_with_classes/main.py
+++ quiz_with_classes/main.py
@@ -1,6 +1,6 @@
 from question_model import QuestionMaker
 from data import question_data
-import random
+from quiz_brain import QuizBrain
 
 #  initialise empty list
 question_object_list = []
@@ -8,23 +8,12 @@ question_object_list = []
 #  iterates through each dictionary in the list, creates a question object containing text and answer
 #  adds the question object to a list
 for question in question_data:
-    question_object = QuestionMaker(question["text"], question["answer"])
+    question_object = QuestionMaker(question["question"], question["correct_answer"])
     question_object_list.append(question_object)
 
-play_again = True
+quiz_object = QuizBrain(question_object_list)
 
-while play_again:
-    question_choice = random.choice(question_object_list)  # chooses a random question object in list
+while quiz_object.still_has_questions():
+    quiz_object.next_question()
 
-    print(question_choice.text)
-    user_answer = input("Answer True or False? ").lower()
-
-    if user_answer == question_choice.answer.lower():
-        print("Well done that is correct!")
-    else:
-        print("Wrong... better luck next time")
-
-    ask_user_to_cont = input("Do you wanna go again? y or n? ")
-
-    if ask_user_to_cont == "n":
-        play_again = False
+print(f"Final score: {quiz_object.score}/{quiz_object.question_number}")
