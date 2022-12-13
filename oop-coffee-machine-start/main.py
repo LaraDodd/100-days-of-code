e76bc3a415e982dac204239ee5729fb17ae238d8
Lara Dodd <laradodd97@gmail.com>
2022-12-13 08:38:38 +0000

    while loop added so that constantly asked until user inputs "off"

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -7,27 +7,27 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 menu = Menu()
 
-
-
-print("Welcome to the coffee machine")
-order = input(f"What would you like? {menu.get_items()}? ")
-
 is_on = True
 
-
 while is_on:
+    print("Welcome to the coffee machine")
+    order = input(f"What would you like? {menu.get_items()}? ")
+
     if order == "off":
         is_on = False
     elif order == "report":
         coffee_maker.report()
         money_machine.report()
     else:
-        menu_item = menu.find_drink(order)
-        if coffee_maker.is_resource_sufficient(menu_item):
+        menu_item_object = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(menu_item_object):
+            print(f"That will cost ${menu_item_object.cost}")
 
-            print(f"That will cost ${menu_item.cost}")
-            if money_machine.make_payment(menu_item.cost):
-                coffee_maker.make_coffee(menu_item)
+            if money_machine.make_payment(menu_item_object.cost):
+                coffee_maker.make_coffee(menu_item_object)
                 coffee_maker.report()
-        is_on = False
+
+
+
+
 
