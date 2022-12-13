fbfa74cf83577a897f3d84c9950227bbeacc3dab
Lara Dodd <laradodd97@gmail.com>
2022-12-13 18:55:13 +0000

    playing around with turtle module spiral circles

--- day_18/circles.py
+++ day_18/circles.py
@@ -2,21 +2,30 @@ import turtle
 from turtle import Turtle, Screen
 import random
 
+
+def random_rgb():
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    return r, g, b
+
+
+def random_colour(object):
+    object.pencolor(random_rgb())
+
+
 turtle.colormode(255)
 laz = Turtle()
 
-laz.shape("classic")
+laz.shape("circle")
+laz.shapesize(.1,.1)
 laz.pensize(1)
-laz.speed(7)
+laz.speed(0)
 
-for i in range(0,360,5):
-
+for i in range(0, 360, 5):
+    laz.pencolor(random_rgb())
     laz.circle(75)
     laz.setheading(i)
-
-
-
-
 
 my_screen = Screen()
 my_screen.exitonclick()
