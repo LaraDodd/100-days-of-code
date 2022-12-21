ecf8c388d226337e59eeaf9b5a9438abb290e00d
Lara Dodd <laradodd97@gmail.com>
2022-12-21 17:21:08 +0000

    adding score feature, need to clear and re call write function in the add score method

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -5,10 +5,16 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.write(f"Score: {self.score}", move=False, align='center', font=('Arial', 8, 'normal'))
+        self.goto(x=0, y=270)
+        self.hideturtle()
+        self.color("white")
+        self.write(f"Score: {self.score}", move=False, align='center', font=('Arial', 20, 'normal'))
 
 
     def add(self):
+        self.clear()
         self.score += 1
+        #  have to call self.write again otherwise it won't re-wrtie the score
+        self.write(f"Score: {self.score}", move=False, align='center', font=('Arial', 20, 'normal'))
 
 
