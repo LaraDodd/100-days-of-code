4f77c64f8e1ef28fe61b62d1acc11dc9268a2b09
Lara Dodd <laradodd97@gmail.com>
2022-12-30 13:12:08 +0000

    adding high score count to acoreboard class

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -10,16 +10,27 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.goto(x=0, y=270)
         self.hideturtle()
         self.color("white")
         self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=SCORE_FONT)
 
-    def increase_score(self):
+    def update_scoreboard(self):
         self.clear()
-        self.score += 1
         #  have to call self.write again otherwise it won't re-wrtie the score
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=SCORE_FONT)
+        self.write(f"Score: {self.score} High ScoreL {self.high_score}", move=False, align=ALIGNMENT, font=SCORE_FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.color("gold")
