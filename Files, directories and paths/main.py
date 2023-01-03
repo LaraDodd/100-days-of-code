c010bcdfbea70619948d14c62ff480858ef0d15f
Lara Dodd <laradodd97@gmail.com>
2023-01-03 22:52:12 +0000

    final commit

--- Files, directories and paths/main.py
+++ Files, directories and paths/main.py
@@ -5,10 +5,21 @@ with open("/Users/larad/Desktop/my_file.txt") as file:
 
 print(contents)
 
-# relative file path
-with open("/Users/larad/Desktop/my_file.txt") as file:
+# relative file path going to the parent folder
+with open("../my_file2.txt") as file:
     contents = file.read()
 
 print(contents)
 
+# relative file path going into a child folder
+with open("./text_files/my_file_3.txt") as file:
+    contents = file.read()
+
+print(contents)
+
+#relative file path going back through a few folders
+with open("../../../../../Desktop/my_file.txt") as file:
+    contents = file.read()
+
+print(contents)
 
