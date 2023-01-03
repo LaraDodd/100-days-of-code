ebb0265d5508aace553fb98723fa9470f33bcf36
Lara Dodd <laradodd97@gmail.com>
2023-01-03 21:21:29 +0000

    new text file added

---
+++ Files, directories and paths/main.py
@@ -0,0 +1,14 @@
+
+#absolute file path
+with open("/Users/larad/Desktop/my_file.txt") as file:
+    contents = file.read()
+
+print(contents)
+
+# relative file path
+with open("/Users/larad/Desktop/my_file.txt") as file:
+    contents = file.read()
+
+print(contents)
+
+
