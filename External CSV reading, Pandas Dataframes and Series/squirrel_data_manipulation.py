3690168ea1256a8d6502d9e4f9e132c3145139c0
Lara Dodd <laradodd97@gmail.com>
2023-01-04 15:24:04 +0000

    reading in csv, pulling out and processing data, writing to dict, creating df from dict, writing to external csv

--- External CSV reading, Pandas Dataframes and Series/squirrel_data_manipulation.py
+++ External CSV reading, Pandas Dataframes and Series/squirrel_data_manipulation.py
@@ -1,4 +1,26 @@
-#read in csv file
+import pandas
 
-#create new df with just: fur colour and number of squirrels
-#print new df to csv
+# read in csv file
+squirrel_df = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+# initialise fur colours list, squirrel count list
+fur_colours = ["Gray", "Cinnamon", "Black"]
+squirrel_count = []
+
+# cycle through fur colours, create filtered dataframe which only has rows of that fur colour, count the number of rows
+for colour in fur_colours:
+    filtered_rows = squirrel_df[squirrel_df["Primary Fur Color"] == colour]
+    squirrel_count.append(filtered_rows.shape[0])  # shape[0] returns no rows, shape[1] returns no. columns
+
+# put into dict
+squirrel_count_dict = {
+    "Fur Colour": fur_colours,
+    "Number of Squirrels": squirrel_count
+}
+
+# convert dict to df
+squirrel_count_df = pandas.DataFrame(squirrel_count_dict)
+print(squirrel_count_df)
+
+# write to csv
+squirrel_count_df.to_csv("squirrel_count.csv")
