a3022b3431b1b4947f05694d14b98cd38ec1337e
Lara Dodd <laradodd97@gmail.com>
2023-01-05 23:43:39 +0000

    test

--- NATO alphabet project (pandas df, dict comprehension)/main.py
+++ NATO alphabet project (pandas df, dict comprehension)/main.py
@@ -6,18 +6,18 @@ nato_data_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict_pandas = {row.letter: row.code for (index, row) in nato_data_df.iterrows()}
 
 """using with open"""
-#read in nato file
+# read in nato file
 with open("nato_phonetic_alphabet.csv", "r") as file:
     nato_data = file.readlines()
 
-#process nato data
+# process nato data
 split_list = [data.split(",") for data in nato_data]
 
-#create a new dictionary using dict comprehension
+# create a new dictionary using dict comprehension
 nato_dict = {split_data[0]: split_data[1].strip("\n") for split_data in split_list if split_data[0] != "letter"}
 
 """rest of code"""
-#Create a list of the phonetic code words from a word that the user inputs.
+# Create a list of the phonetic code words from a word that the user inputs.
 
 word = input("Type a word ").upper()
 
@@ -27,4 +27,3 @@ print(nato_list)
 
 nato_list_pd = [nato_dict_pandas[letter] for letter in word]
 print(nato_list_pd)
-
