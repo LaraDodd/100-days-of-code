d0b94471fef5a3a79e6b4150699d9238f4ba46ac
Lara Dodd <laradodd97@gmail.com>
2023-01-10 13:35:02 +0000

    added following functions: restart: resets the timer to a timer segment and prints degment info, depending on rep number add check: adds a check mark to GUI depending on rep number reset: resets rep number, resets all text widgets start timer: called by start button. Calls the update time function and passes in a parameter for the minutes argument based on the rep number. update time: uses window.after() to call itself every second and decrease the passed in second argument parameter by 1, and the minute arg parameter by 1/60. re-writes to timer_text in canvas every second. When minutes become negative, calls the restart function and adds 1 to the rep number.

--- Tkinter, Dynamic typing and the Pomodoo GUI app project/main.py
+++ Tkinter, Dynamic typing and the Pomodoo GUI app project/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import time
+import math
 
 # ---------------------------- GLOBAL CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,37 +10,88 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 1
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-def reset_button():
-    min = 25
+def restart():
+    if REPS % 2 != 0 and REPS < 8:
+        minutes = WORK_MIN
+        break_type_label.config(text="Work!")
+    elif REPS < 8:
+        minutes = SHORT_BREAK_MIN
+        break_type_label.config(text="Break!")
+    elif REPS == 8:
+        minutes = LONG_BREAK_MIN
+        break_type_label.config(text="LOOOOOOONG BREAK!")
+    else:
+        minutes = "00"
+        break_type_label.config(text="DONE")
+
+    add_check()
+    canvas.itemconfig(text, text=f"{minutes}:00")
 
 
-# clear canvas and update
-
-# ---------------------------- TIMER MECHANISM ------------------------------- #
-
-
-
-
-def update_time(seconds):
-    print(seconds)
-    window.after(1000, update_time, seconds-1)
+def reset():
+    global REPS
+    REPS = 1
+    break_type_label.config(text="")
+    canvas.itemconfig(text, text=f"00:00")
+    check_label.config(text="")
 
 
-    # canvas.delete()
-    # canvas.create_text(100, 130, text=f"{str(minutes)}:{str(seconds)}", fill="white", font=("courier", 25, "bold"),
-    #                    anchor="center")
+def add_check():
+    global REPS
+    if REPS >= 8:
+        check_label.config(text="✔✔✔✔")
+    elif REPS > 6:
+        check_label.config(text="✔✔✔")
+    elif REPS > 4:
+        check_label.config(text="✔✔")
+    elif REPS > 2:
+        check_label.config(text="✔")
 
 
+# ---------------------------- TIMER ------------------------------- #
+def start_timer():
+    global REPS
+
+    if REPS % 2 != 0 and REPS < 8:
+        minutes = WORK_MIN
+    elif REPS < 8:
+        minutes = SHORT_BREAK_MIN
+    elif REPS == 8:
+        minutes = LONG_BREAK_MIN
+    else:
+        restart()
+
+    update_time(minutes=minutes - 1, seconds=0)
 
 
+# ---------------------------- COUNTDOWN ------------------------------- #
+def update_time(seconds, minutes):
+    if seconds < 0:
+        seconds = 59
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def update_checks():
-    checks = 1
-    check_label.config(text=checks)
+    pretty_seconds = seconds
+    if seconds < 10:
+        pretty_seconds = "0" + str(seconds)
+
+    pretty_minutes = math.ceil(minutes)
+    if pretty_minutes < 0:
+        global REPS
+        REPS += 1
+        restart()
+
+        print(REPS)
+
+        return
+
+    if minutes < 9:
+        pretty_minutes = "0" + str(math.ceil(minutes))
+
+    window.after(1, update_time, seconds - 1, minutes - (1 / 60))
+    canvas.itemconfig(text, text=f"{pretty_minutes}:{pretty_seconds}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -50,7 +101,7 @@ window.title("Pomodoro Clock")
 window.config(padx=100, pady=100)
 window.configure(background=YELLOW)
 
-#timer label
+# timer label
 label = Label(fg=GREEN, text="Timer", font=("courier", 40, "bold"), bg=YELLOW)
 label.config(pady=10, padx=10)
 label.grid(row=0, column=1, sticky="EW")
@@ -62,32 +113,23 @@ image = canvas.create_image(100, 112, anchor="center", image=filename)
 canvas.grid(row=1, column=1)
 
 # add text
-sec = "00"
-min = "00"
-text = canvas.create_text(100, 130, text=f"{min}:{sec}", fill="white", font=("courier", 25, "bold"), anchor="center")
+text = canvas.create_text(100, 130, text=f"00:00", fill="white", font=("courier", 25, "bold"), anchor="center")
 canvas.grid(row=1, column=1)
 
 # add buttons
-start_button = Button(window, text="Start", bg="White")
+# lambda allows you to call functions with parameters
+start_button = Button(window, text="Start", bg="White", command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(window, text="Reset", bg="White")
+reset_button = Button(window, text="Reset", bg="White", command=reset)
 reset_button.grid(row=2, column=2)
 
-#add check labels
-check_label = Label(window, text="✔", font=("courier", 20, "bold"), bg=YELLOW, fg=GREEN)
+# add check labels
+check_label = Label(window, text="", font=("courier", 20, "bold"), bg=YELLOW, fg=GREEN)
 check_label.grid(row=3, column=1)
 
-
-
+# add info label
+break_type_label = Label(window, text="", font=("courier", 15, "bold"), bg=YELLOW, fg=PINK)
+break_type_label.grid(row=4, column=1, sticky="EW")
 
 window.mainloop()
-
-
-
-update_time(seconds)
-
-
-
-
-
