4f83a81ddd374ceab5507ec32c9246d9c4b2a0e4
Lara Dodd <laradodd97@gmail.com>
2023-01-11 14:34:36 +0000

    small aesthetic changes and adding focus to website entry so cursor starts in it

--- Password Manager App (using tkinter GUI and external database writing)/main.py
+++ Password Manager App (using tkinter GUI and external database writing)/main.py
@@ -52,21 +52,22 @@ window.config(padx=30, pady=30)
 window.configure(background='#fffcf7')
 
 # creating label components
-website_label = Label(text="Website:", font=("arial", 12, "normal"), bg="#fffcf7", fg="#738290")
+website_label = Label(text="Website:", font=("arial", 12, "normal"), bg="#fffcf7", fg="#555b6e")
 website_label.grid(column=0, row=1, sticky="E")
 website_label.config(padx=5, pady=5)
 
-username_label = Label(text="Email / Username: ", font=("arial", 12, "normal"), bg="#fffcf7", fg="#738290")
+username_label = Label(text="Email / Username: ", font=("arial", 12, "normal"), bg="#fffcf7", fg="#555b6e")
 username_label.grid(column=0, row=2, sticky="E")
 username_label.config(padx=5, pady=5)
 
-password_label = Label(text="Password: ", font=("arial", 12, "normal"), bg="#fffcf7", fg="#738290")
+password_label = Label(text="Password: ", font=("arial", 12, "normal"), bg="#fffcf7", fg="#555b6e")
 password_label.grid(column=0, row=3, sticky="E")
 password_label.config(padx=5, pady=10)
 
 # creating entry boxes
 website_input = Entry(width=50, highlightcolor="#a1b5d8", highlightthickness=2, border=1)
 website_input.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_input.focus()
 
 username_input = Entry(width=50, highlightcolor="#a1b5d8", highlightthickness=2, border=1)
 username_input.grid(column=1, row=2, columnspan=2, sticky="EW")
@@ -75,17 +76,17 @@ password_input = Entry(width=30, highlightcolor="#a1b5d8", highlightthickness=2,
 password_input.grid(column=1, row=3, sticky="W")
 
 # creating button components
-add_button = Button(text="Add", bg="#c2d8b9", fg="#738290", command=get_data_and_add)
+add_button = Button(text="Add", bg="#c2d8b9", fg="#555b6e", command=get_data_and_add)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
-gen_pass_button = Button(text="Generate Password", bg="#c2d8b9", fg="#738290",
+gen_pass_button = Button(text="Generate Password", bg="#c2d8b9", fg="#555b6e",
                          command=generate_password_click)  # give command when clicked
 gen_pass_button.grid(row=3, column=2, sticky="E")
 
 # create canvas and add padlock image
-canvas = Canvas(window, bg="#fffcf7", height=200, width=190, highlightthickness=0, relief='ridge')
+canvas = Canvas(window, bg="#fffcf7", height=200, width=200, highlightthickness=0, relief='ridge')
 filename = PhotoImage(file="logo.png")
-image = canvas.create_image(100, 112, anchor="center", image=filename)
+image = canvas.create_image(100, 100, anchor="center", image=filename)
 canvas.grid(row=0, column=0, columnspan=3)
 
 window.mainloop()
