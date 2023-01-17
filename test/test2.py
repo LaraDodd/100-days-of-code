5276833c30c452bfa6c618774f7ee43fc55ef037
Lara Dodd <laradodd97@gmail.com>
2023-01-17 17:56:17 +0000

    adding menu

--- test/test2.py
+++ test/test2.py
@@ -39,22 +39,21 @@ class Shoe:
         return output
 
 
-# =============Shoe list===========
-'''
-The list will be used to store a list of objects of shoes.
-'''
-shoe_list = []
+# =============initial variables===========
+
+shoe_list = []  # used to store a list of objects of shoes
+shoes_df = pandas.read_csv("inventory.csv")  # stores csv data
 
 
 # =============Functions outside the class==============
+
 def read_shoes_data():
     """This function will open the file inventory.csv in a pd dataframe
     and read the data from this file, then create a shoes object with this data
     and append this object into the shoes list."""
-    shoes_df = pandas.read_csv("inventory.csv")
     shoes_dict = shoes_df.to_dict(orient="records")
     for data in shoes_dict:
-        shoe_object = Shoe(data["Country"], data["Code"], data["Product"], data["Cost"], data["Quantity"])
+        shoe_object = Shoe(data["Country"], data["Code"], data["Product"], data["Cost (£)"], data["Quantity"])
         shoe_list.append(shoe_object)
     return shoes_dict
 
@@ -78,7 +77,7 @@ def capture_shoes():
                       'Quantity': quantity,
                       }
 
-    shoes_dict_list = read_shoes_data()
+    shoes_dict_list = read_shoes_data()  # get shoes_dict_list by callind read shoes data method
     shoes_dict_list.append(data_dict_item)
 
     df = pandas.DataFrame(shoes_dict_list)
@@ -101,10 +100,7 @@ def view_all():
     #     print(shoe.__str__())
 
     # leaving above for marker but using pd df as cleaner:
-    shoes_dict_list = read_shoes_data()
-
-    df = pandas.DataFrame(shoes_dict_list)
-    print(df)
+    print(shoes_df)
 
 
 def re_stock():
@@ -114,26 +110,28 @@ def re_stock():
     want to add this quantity of shoes and then update it.
     This quantity should be updated on the file for this shoe.
     '''
-    shoes_df = pandas.read_csv("inventory.csv")
+
     min_qty_product = shoes_df[shoes_df.Quantity == shoes_df.Quantity.min()]
     print(f"The product/s with the lowest stock is/are:\n{min_qty_product}\n")
 
-    try:
-        quantity = int(input("Enter the number that you would like to restock product by "))
-    except TypeError:
-        print("This is not a number")
-        re_stock()
-    else:
-        new_quantity = shoes_df.Quantity.min()+quantity
-    finally:
-        index = shoes_df.index[shoes_df.Quantity == shoes_df.Quantity.min()]
-        shoes_df.loc[index, "Quantity"] = new_quantity
+    sku_false = True
+    while sku_false:
 
-    #write to csv
+        try:
+            quantity = int(input("Enter the number that you would like to restock product by "))
+        except TypeError:
+            print("This is not a number")
+
+        else:
+            new_quantity = shoes_df.Quantity.min() + quantity
+            index = shoes_df.index[shoes_df.Quantity == shoes_df.Quantity.min()]
+            shoes_df.loc[index, "Quantity"] = new_quantity
+            sku_false = False
+
+    # write to csv
     shoes_df.to_csv("inventory.csv", index=False)
 
-    print(f"The new quantity of {min_qty_product.Product.to_string(index=False)} is {new_quantity}")
-
+    print(f"The new quantity of:\n {min_qty_product.Product.to_string(index=False)}\n is {new_quantity}")
 
 
 def search_shoe():
@@ -142,22 +140,49 @@ def search_shoe():
      using the shoe code and return this object so that it will be printed.
     '''
 
+    sku_code = input("Please enter the SKUcode of the item you would like to find details about: ")
+
+    # read_shoes_data()
+    # for shoe_object in shoe_list:
+    #     try:
+    #         if sku_code == shoe_object.code:
+    #             selected_shoe = shoe_object
+    #             test = selected_shoe.__str__()
+    #             print(test)
+    #     except:
+    #         print("sku not in sys")
+
+    # cleaner using pd
+
+    product = shoes_df[shoes_df.Code == sku_code]
+    if len(product) == 0:
+        print("This is not a valid SKU")
+        search_shoe()
+
+    else:
+        print(product)
+
 
 def value_per_item():
-    pass
     '''
     This function will calculate the total value for each item.
     Please keep the formula for value in mind: value = cost * quantity.
     Print this information on the console for all the shoes.
     '''
+    print("\n")
+    shoes_df["Value (£)"] = shoes_df["Cost (£)"] * shoes_df["Quantity"]
+    print(shoes_df)
 
 
 def highest_qty():
-    pass
     '''
     Write code to determine the product with the highest quantity and
     print this shoe as being for sale.
     '''
+
+    max_qty_product = shoes_df[shoes_df.Quantity == shoes_df.Quantity.max()]
+    print(f"As we have {shoes_df.Quantity.max()} of the {max_qty_product.Product.to_string(index=False)}, "
+          f"they are going on SALE!")
 
 
 # ==========Main Menu=============
@@ -165,16 +190,37 @@ def highest_qty():
 Create a menu that executes each function above.
 This menu should be inside the while loop. Be creative!
 '''
-# shoe = Shoe("UK", "2", "3", "4", "5")
-# print(type(shoe))
-# print(shoe.product)
-#
-# shoes = read_shoes_data()
-#
-# for shoe in shoes:
-#     print(shoe.product)
 
+while True:
+    print("\n")
+    menu = input("Select one of the following Options below:\n"
+                 "va - View all products\n"
+                 "a - Add product to database\n"
+                 "rs - re-stock lowest quantity items\n"
+                 "se - search for specific product details\n"
+                 "sa - View sale product\n"
+                 "£££ - View value amount of each product\n"
+                 "e - Exit\n"
+                 ":")
 
-view_all()
-print()
-re_stock()
+    if menu == "va":
+        view_all()
+
+    elif menu == "a":
+        capture_shoes()
+
+    elif menu == "rs":
+        re_stock()
+
+    elif menu == "se":
+        search_shoe()
+
+    elif menu == "sa":
+        highest_qty()
+
+    elif menu == "£££":
+        value_per_item()
+
+    elif menu == 'e':
+        print('Goodbye!!!')
+        exit()
