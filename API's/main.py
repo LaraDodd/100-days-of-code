e3df87b424a974ea983d752b85ccf0b6eb678a10
Lara Dodd <laradodd97@gmail.com>
2023-01-20 13:11:10 +0000

    final commit of Kayne app

--- API's/main.py
+++ API's/main.py
@@ -1,24 +1,43 @@
 from tkinter import *
+import requests
 
 #==========FUNCTIONS===========
 def get_quote():
-    pass
+    initial_label.config(text="")
+    response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status() # raise exception if error occurs
+    quote_data = response.json()
+    quote = quote_data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
 
 
 #=============UI===============
 window = Tk()
 window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg="white")
 
-canvas = Canvas(width=300, height=414)
+#create canvas
+canvas = Canvas(width=300, height=414, bg="white", highlightthickness=0)
+
+#add background image to cnvas
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+
+# add quote text to canvas
+quote_text = canvas.create_text(150, 207, text="...", width=250, font=("Arial", 25, "bold"),
+                               fill="white")
+# place canvas
 canvas.grid(row=0, column=0)
 
+#add photo to button, place button
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote, bg="white", border=0)
 kanye_button.grid(row=1, column=0)
+
+# add initial click me
+initial_label = Label(text="Click my head^^^", font=("Arial", 10), fg="black", bg="white")
+initial_label.grid(row=2, column=0)
+
 
 
 
