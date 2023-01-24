19aba1046cc29487cd950b24d3918759bc819108
Lara Dodd <laradodd97@gmail.com>
2023-01-24 23:54:33 +0000

    adding in end of quiz functionaility with if statement checking whether question left. If no q's left, score printed and buttons disabled

--- Quizzler App with UI as class/ui.py
+++ Quizzler App with UI as class/ui.py
@@ -2,10 +2,10 @@ THEME_COLOR = "#375362"
 from tkinter import *
 from quiz_brain import QuizBrain
 
+
 class QuizInterface():
     def __init__(self, quiz_object: QuizBrain):
         self.quiz = quiz_object
-
 
         self.window = Tk()
         self.window.title("Quizzle")
@@ -22,25 +22,37 @@ class QuizInterface():
                                                  width=350)
         self.canvas.grid(row=1, column=0, columnspan=2)
 
+        self.score_text = Label(text=f"Score: {self.quiz.score}", bg=THEME_COLOR, fg="white",
+                                font=("Ariel", 15, "italic"))
+        self.score_text.grid(row=0, column=1)
+
         cross_image = PhotoImage(file="images/false.png")
-        wrong_button = Button(image=cross_image, highlightthickness=0, command=self.false_selected) # , command=false_ticked
-        wrong_button.grid(row=2, column=0)
+        self.wrong_button = Button(image=cross_image, highlightthickness=0,
+                              command=self.false_selected)  # , command=false_ticked
+        self.wrong_button.grid(row=2, column=0)
 
         check_image = PhotoImage(file="images/true.png")
-        right_button = Button(image=check_image, highlightthickness=0, command=self.true_selected) # , command=true_ticked
-        right_button.grid(row=2, column=1)
+        self.right_button = Button(image=check_image, highlightthickness=0,
+                              command=self.true_selected)  # , command=true_ticked
+        self.right_button.grid(row=2, column=1)
 
         self.get_next_question()
 
-        #you can't call specific attributes from a passed in class, you can only call functions from it
-
+        # you can't call specific attributes from a passed in class, you can only call functions from it
 
         self.window.mainloop()
 
     # call next question from quiz brain. This returns the question as a string
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(tagOrId=self.quiz_text, text=q_text)
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(tagOrId=self.quiz_text, text=q_text)
+            self.score_text.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.itemconfig(tagOrId=self.quiz_text, text=f"You've reached the end of the quiz, your score was"
+                                                                f" {self.quiz.score}/{self.quiz.question_number}")
+            self.wrong_button.config(state="disabled")
+            self.right_button.config(state="disabled")
 
     def false_selected(self):
         # q_text = self.quiz.next_question()
@@ -73,8 +85,3 @@ class QuizInterface():
         self.canvas.config(bg="red")
         self.window.config(bg="red")
         self.window.after(100, self.go_blue)
-
-
-
-
-
