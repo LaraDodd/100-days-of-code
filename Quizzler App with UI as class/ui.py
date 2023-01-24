ba2ed02a0951a1c9ea1a8ba5bd4665c9cc4c1636
Lara Dodd <laradodd97@gmail.com>
2023-01-24 22:46:43 +0000

    created get_next_question method which calls the next_question method from QuizBrain class (this returns current question as a string and adds 1 to question number). This then rewrites canvas.q_text in ui as the returned question text.

--- Quizzler App with UI as class/ui.py
+++ Quizzler App with UI as class/ui.py
@@ -1,11 +1,11 @@
 THEME_COLOR = "#375362"
 from tkinter import *
-import quiz_brain
+from quiz_brain import QuizBrain
 
 class QuizInterface():
-    def __init__(self, quiz_object: quiz_brain):
+    def __init__(self, quiz_object: QuizBrain):
         self.quiz = quiz_object
-        self.q_text = self.get_next_question()
+
 
         self.window = Tk()
         self.window.title("Quizzle")
@@ -17,18 +17,20 @@ class QuizInterface():
         self.canvas_background = self.canvas.create_image(300, 263, image=quiz_bg_img)
         self.quiz_text = self.canvas.create_text(300,
                                                  200,
-                                                 text=self.q_text,
+                                                 text="",
                                                  font=("Ariel", 20, "italic"),
                                                  width=350)
         self.canvas.grid(row=1, column=0, columnspan=2)
 
         cross_image = PhotoImage(file="images/false.png")
-        wrong_button = Button(image=cross_image, highlightthickness=0) # , command=false_ticked
+        wrong_button = Button(image=cross_image, highlightthickness=0, command=self.get_next_question) # , command=false_ticked
         wrong_button.grid(row=2, column=0)
 
         check_image = PhotoImage(file="images/true.png")
-        right_button = Button(image=check_image, highlightthickness=0) # , command=true_ticked
+        right_button = Button(image=check_image, highlightthickness=0, command=self.get_next_question) # , command=true_ticked
         right_button.grid(row=2, column=1)
+
+        self.get_next_question()
 
         #you can't call specific attributes from a passed in class, you can only call functions from it
 
@@ -37,11 +39,18 @@ class QuizInterface():
 
     # call next question from quiz brain. This returns the question as a string
     def get_next_question(self):
-        return self.quiz.next_question()
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(tagOrId=self.quiz_text, text=q_text)
 
     def false_selected(self):
-        pass
+        self.quiz.next_question()
+        user_answer = "false"
+        is_correct = self.quiz.check_answer(user_answer)
+        return is_correct
 
     def true_selected(self):
-        pass
+        self.quiz.next_question()
+        user_answer = "true"
+        is_correct = self.quiz.check_answer(user_answer)
+        return is_correct
 
