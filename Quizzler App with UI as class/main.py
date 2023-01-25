f35af669850f1234ab199bd40cfc06880d9b7dc1
Lara Dodd <laradodd97@gmail.com>
2023-01-25 09:24:29 +0000

    adding doc straings and type annotations to question model module, data module, quiz_brain module and main

--- Quizzler App with UI as class/main.py
+++ Quizzler App with UI as class/main.py
@@ -3,7 +3,7 @@ from data import question_data
 from quiz_brain import QuizBrain
 from ui import QuizInterface
 
-#create list of question objects using Question class from question model module
+# create list of question objects using Question class from question model module
 question_bank = []
 for question in question_data:
     question_text = question["question"]
@@ -11,9 +11,8 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-#create quiz object from QuizBrain class with list of question objects as input
+# create quiz object from QuizBrain class with list of question objects as input
 quiz = QuizBrain(question_bank)
 
-#create quiz ui object from QuizInterface class with quiz object as input
+# create quiz ui object from QuizInterface class with quiz object as input
 quiz_ui = QuizInterface(quiz)
-
