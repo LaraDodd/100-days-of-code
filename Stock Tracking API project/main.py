7f2c92efe144271b41d0590bab9c373f0ebe7169
Lara Dodd <laradodd97@gmail.com>
2023-02-01 14:35:42 +0000

    cleaning up

--- Stock Tracking API project/main.py
+++ Stock Tracking API project/main.py
@@ -18,15 +18,3 @@ if significant_percentage_change(today_percentage_difference):
     print(tesla_tracker_brain.write_text())
     tesla_tracker_brain.send_text()
 
-# netflix_data = Data(stock_name="Netflix", stock_symbol="NFLX")
-# netflix_tracker_brain = StockTrackerBrain(stock_name="Netflix", stock_symbol="NFLX") #need to figure out a way to pass in
-# # data object instead of just attributes again
-#
-# # call function to find percentage difference
-# today_percentage_difference = netflix_tracker_brain.percentage_change
-#
-# # if the percentage difference is significant call get news function
-# if significant_percentage_change(today_percentage_difference):
-#     print(f"percentage difference: {today_percentage_difference}%")
-#     print(netflix_tracker_brain.write_text())
-#     netflix_tracker_brain.send_text()
